83c2d49c8c74dfa9ad8321e5a364213c1ae1451c
Ho Tien Ky <hothanh1072@gmail.com>
2023-03-27 20:59:43 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\v1\AuthController;
 use App\Http\Controllers\v1\ProductController;
 use App\Http\Controllers\v1\ProductVariantController;
 use App\Http\Controllers\v1\StoreController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +26,49 @@ Route::get('ping', function () {
 
 Route::get('/', function () {
     return api_success('welcome to om healing app');
+});
+
+Route::prefix('/public/employee_otp/')->group(function ($route) {
+    Route::post('resend', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required'
+        ], [
+            'required' => 'The :attribute field is required.',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                "code" => 400,
+                "name" => "Bad Request",
+                "description" => "<p>Cannot find user from phone number.</p>"
+            ]);
+        }
+        return response()->json([
+            "code" => 200,
+            "status" =>  true,
+            "data" => $request->all()
+        ]);
+    });
+    Route::post('auth', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required',
+            'otp_code' => 'required|digits:6'
+        ], [
+            'required' => 'The :attribute field is required.',
+            'digits' => ':attribute must have 6 digits'
+        ]);
+        if ($validator->fails() || $request->get('otp') !== '111111') {
+            return response()->json([
+                "code" => 400,
+                "name" => "Bad Request",
+                "description" => "Error"
+            ]);
+        }
+        return response()->json([
+            "code" => 200,
+            "status" =>  true,
+            "data" => $validator
+        ]);
+    });
 });
 
 Route::prefix('v1')->group(function () {
